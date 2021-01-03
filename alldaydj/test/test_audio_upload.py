17f360a031f754ae147d01907f9c26edd3126469
Marc Steele <steelegbr@gmail.com>
2021-01-03 15:22:59 +0000

    ADDJ-9 Fixing for the linter.

--- alldaydj/test/test_audio_upload.py
+++ alldaydj/test/test_audio_upload.py
@@ -179,7 +179,14 @@ class AudioUploadTests(APITestCase):
 
         return job
 
-    @parameterized.expand([("./alldaydj/test/files/invalid_type.txt", "ASCII text, with no line terminators")])
+    @parameterized.expand(
+        [
+            (
+                "./alldaydj/test/files/invalid_type.txt",
+                "ASCII text, with no line terminators",
+            )
+        ]
+    )
     @patch("django.core.files.storage.default_storage.delete")
     @patch("django.core.files.storage.default_storage.open")
     def test_validate_invalid_file(

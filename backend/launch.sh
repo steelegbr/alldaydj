c988b609ef824532b7e9ad21199811e623232d74
Marc Steele <steelegbr@gmail.com>
2021-01-18 08:18:50 +0000

    ADDJ-30 Attempt to fix auto detection of celery jobs.

--- backend/launch.sh
+++ backend/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
-celery -A alldaydj multi start worker1 --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
+celery -A alldaydj.celery multi start worker1 --loglevel=DEBUG --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
 python manage.py runserver 0.0.0.0:8000

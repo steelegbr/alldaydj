f5c3cc50a3663f817db4ee4ed80b20eea2e312a0
Marc Steele <steelegbr@gmail.com>
2021-01-18 07:45:43 +0000

    ADDJ-30 Adding logging to the launcher.

--- backend/launch.sh
+++ backend/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate
-celery -A alldaydj multi start worker1
+celery -A alldaydj multi start worker1 -pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
 python manage.py runserver 0.0.0.0:8000

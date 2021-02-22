f51071857b63c1ebbae209650c323f2ee856702b
Marc Steele <steelegbr@gmail.com>
2021-02-22 21:57:31 +0000

    ADDJ-4 Added test for automatic logout trigger.

--- frontend/alldaydj/src/pages/Authentication/Logout.test.tsx
+++ frontend/alldaydj/src/pages/Authentication/Logout.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Logout from './Logout';
+import { AuthenticationContext, AuthenticationStage, AuthenticationStatusProps } from '../../components/context/AuthenticationContext';
 
 const mockPush = jest.fn();
+const mockSetAuthenticationStatus = jest.fn();
 
 jest.mock('react-router-dom', () => ({
   useHistory: () => ({
@@ -11,12 +13,29 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('logout page', () => {
-  const getComponent = () => mount(<Logout />);
+  const getComponent = (stage: AuthenticationStage) => {
+    const authContextValue : AuthenticationStatusProps = {
+      authenticationStatus: {
+        stage,
+      },
+      setAuthenticationStatus: mockSetAuthenticationStatus,
+    };
+    return mount(
+      <AuthenticationContext.Provider value={authContextValue}>
+        <Logout />
+      </AuthenticationContext.Provider>,
+    );
+  };
 
   it('clicking log back in redirects', () => {
-    const component = getComponent();
+    const component = getComponent('Unauthenticated');
     const loginButton = component.find("[data-test='button-login']").first();
     loginButton.simulate('click');
     expect(mockPush).toBeCalledWith('/login/');
+  });
+
+  it('automatic log out triggers', () => {
+    getComponent('Authenticated');
+    expect(mockSetAuthenticationStatus).toBeCalledWith({ stage: 'Unauthenticated' });
   });
 });

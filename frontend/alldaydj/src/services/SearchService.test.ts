c4aa8d189f0f2e96a6faa19a232a52238af7ebbf
Marc Steele <steelegbr@gmail.com>
2021-03-03 18:58:40 +0000

    ADDJ-4 Improved test coverage.

--- frontend/alldaydj/src/services/SearchService.test.ts
+++ frontend/alldaydj/src/services/SearchService.test.ts
@@ -1,4 +1,4 @@
-import { paramsToSearchConditions } from 'services/SearchService';
+import { cartSearchContextFromQueryString, paramsToSearchConditions } from 'services/SearchService';
 
 describe('cart search service', () => {
   it('querystring params to search conditions empty', () => {
@@ -14,14 +14,27 @@ describe('cart search service', () => {
   });
 
   it('querystring params to search conditions valid', () => {
-    const converted = paramsToSearchConditions(new URLSearchParams());
-    expect(converted).toStrictEqual({
-      advanced: 'false',
-      artist: '',
-      page: '1',
-      resultsPerPage: '10',
-      search: '',
-      title: '',
-    });
+    const params : Record<string, string> = {
+      advanced: 'true',
+      artist: 'The Artist',
+      page: '5',
+      resultsPerPage: '7',
+      search: 'something',
+      title: 'A Title',
+    };
+    const converted = cartSearchContextFromQueryString(new URLSearchParams(params));
+    expect(converted).toStrictEqual(
+      {
+        conditions: {
+          advanced: 'true',
+          artist: 'The Artist',
+          page: '5',
+          resultsPerPage: '7',
+          search: 'something',
+          title: 'A Title',
+        },
+        status: 'ReadyToSearch',
+      },
+    );
   });
 });

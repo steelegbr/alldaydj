c362fb09c2dd00b1518963d6176192c2a66a1546
Marc Steele <steelegbr@gmail.com>
2021-11-07 20:59:09 +0000

    Restore gunicorn config.

--- backend/launch.sh
+++ backend/launch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python manage.py migrate
 celery -A alldaydj multi start worker1 --loglevel=INFO --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
-#gunicorn -b 0.0.0.0:8000 alldaydj.wsgi --timeout 300 --workers 3 --log-level=debug
-python manage.py runserver 0.0.0.0:8000
+gunicorn -b 0.0.0.0:8000 alldaydj.wsgi --timeout 300 --workers 3 --log-level=debug

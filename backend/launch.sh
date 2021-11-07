9f83ceb9bb08c33fe278c84bcd7c172bde5671d5
Marc Steele <steelegbr@gmail.com>
2021-11-07 20:45:59 +0000

    Turns out we have to allow the world to talk.

--- backend/launch.sh
+++ backend/launch.sh
@@ -2,4 +2,4 @@
 python manage.py migrate
 celery -A alldaydj multi start worker1 --loglevel=INFO --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
 #gunicorn -b 0.0.0.0:8000 alldaydj.wsgi --timeout 300 --workers 3 --log-level=debug
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000

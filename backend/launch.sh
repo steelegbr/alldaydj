19e91e742dd8576de2cdcefcdec0e96440e69027
Marc Steele <steelegbr@gmail.com>
2022-05-19 18:50:17 +0100

    Clear out to start again with FastAPI and GCP.

--- backend/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python manage.py migrate
-celery -A alldaydj multi start worker1 --loglevel=INFO --pidfile=/var/run/celery/%n.pid --logfile=/var/log/celery/%n%I.log
-gunicorn -b 0.0.0.0:8000 alldaydj.wsgi --timeout 300 --workers 3 --log-level=debug

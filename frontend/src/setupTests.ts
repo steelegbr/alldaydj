ae96e520d63664f0dd6d2a1231eaa70c0566941c
Marc Steele <marc@steama.co>
2024-09-14 18:31:45 +0100

    Clear out the old codebase.

--- frontend/src/setupTests.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import '@testing-library/jest-dom';
-import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import 'jest-date-mock';
-
-require('jest-localstorage-mock');
-
-configure({ adapter: new Adapter() });

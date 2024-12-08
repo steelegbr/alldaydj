ea3c592aa720c95e6e5c7f3b19c1f48507b98742
Marc Steele <steelegbr@gmail.com>
2024-12-08 09:51:15 +0000

    Start again with a fresh repo

--- AllDayDJBackend/Models/User.cs
+++
@@ -1,7 +0,0 @@
-namespace AllDayDJBackend.Models;
-
-public class User
-{
-    public Guid Id { get; set; }
-    public string Email { get; set; }
-}
--- AllDayDJBackend/Repository/Interfaces/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-using AllDayDJBackend.Models;
-
-namespace AllDayDJBackend.Repository;
-
-public interface IUserRepository : IDisposable
-{
-    public User GetOrCreateUser(string email);
-}

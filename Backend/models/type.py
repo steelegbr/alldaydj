9d24192c75a303e84f04db2071653a20f7d40dfc
Marc Steele <steelegbr@gmail.com>
2025-02-11 19:19:21 +0000

    Add genre and cart type

---
+++ Backend/models/type.py
@@ -0,0 +1,10 @@
+from models.mongodb import PyObjectId
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class CartType(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    cart_type: str
+
+class CartTypeUpdate(BaseModel):
+    cart_type: str
